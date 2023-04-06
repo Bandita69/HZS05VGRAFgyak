64df2c371895a2adc032243fb794a4626ca4d193
Bandita69 <15244649+Bandita69@users.noreply.github.com>
2023-04-06 11:59:21 +0200

    4.gyak készen van

--- gyakorlatok/gyak04/main.c
+++ gyakorlatok/gyak04/main.c
@@ -5,12 +5,14 @@
 /**
  * Main function
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     App app;
     app.toggle = 1;
+    printf("1- heart 2-mandarin 3-haromszog 4-piramis 5-sakktabla 6-henger 7-kup\n");
     init_app(&app, 800, 600);
-    while (app.is_running) {
+    while (app.is_running)
+    {
         handle_app_events(&app);
         update_app(&app);
         render_app(&app);
